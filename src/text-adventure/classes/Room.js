12b22c7461cdc855f12f42e5399a44861cefefb8
shivam79315 <158444124+shivam79315@users.noreply.github.com>
2025-11-13 00:52:27 +0530

    Updated Room.js -- added description(minor change)

--- src/text-adventure/classes/Room.js
+++ src/text-adventure/classes/Room.js
@@ -1,12 +1,14 @@
 export default class Room {
-    constructor(name ="Unnamed Room", items = [], exits = {}) {
+    constructor(name ="Unnamed Room", items = [], exits = {}, description = "No description available.") {
         this.name = name;
         this.items = items;
         this.exits = exits;
+        this.description = description;
     }
 
     showDetails() {
         console.log(`\n=== ${this.name} Room ===`);
+        console.log(`Description: ${this.description} \n`);
         if(this.items.length > 0) {
             console.log("Items here are:");
             this.items.forEach(item => console.log(`- ${item}`));
